3471b3143017756738a3ce271e5bfff0d8a64911
Marwan Sabry <shobo12maro@hotmail.com>
2022-12-13 20:39:43 +0000

    added assemblyscript functions

--- assembly/index.ts
+++ assembly/index.ts
@@ -3,3 +3,17 @@
 export function add(a: i32, b: i32): i32 {
   return a + b;
 }
+
+export function fizzbuzz(n: i32): string {
+  if(n % 15 == 0)
+    return "fizzbuzz";
+  if(n % 5 == 0)
+    return "buzz";
+  if(n % 3 == 0)
+    return "fizz";
+  return n.toString();
+}
+
+export function fibb(n: i32): i32 {
+  return (n == 1 || n == 0) ? 1 : fibb(n-1) + fibb(n-2);
+}
